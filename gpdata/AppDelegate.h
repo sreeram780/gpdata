beda39c1da3ae3f0592a3f3ff8d22afc80495d14
sreeram780 <sreenu.ram2@gmail.com>
2015-11-04 18:52:47 +0530

    Initial Commit

---
+++ gpdata/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gpdata
+//
+//  Created by sreenivasulareddy on 04/11/15.
+//  Copyright © 2015 sreenivasulareddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gpdata/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gpdata
+//
+//  Created by sreenivasulareddy on 04/11/15.
+//  Copyright © 2015 sreenivasulareddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
